ee8bd6d2542662f0655dff2d17cd67fee5d96db9
Marcel Wagner <wagmarcel@web.de>
2018-08-16 17:54:46 +0200

    Revert "Added 30s before heartbeat is sent. Looks like heartbeat is indicating readiness too early otherwise. In future this has to be done configurable"
    This reverts commit 9e89563b16049d0696d7605c67d1a0b4c70eee4c.

--- src/main/java/org/oisp/pipeline/FullPipelineBuilder.java
+++ src/main/java/org/oisp/pipeline/FullPipelineBuilder.java
@@ -38,10 +38,8 @@ public class FullPipelineBuilder {
         @DoFn.ProcessElement
         public void processElement(ProcessContext c) {
             Long seqnum = c.element();
-            if (seqnum > 30 ) { // wait 30 seconds before sending heartbeats
-                KV<String, String> out_kv = KV.<String, String>of("", "rules-engine");
-                c.output(out_kv);
-            }
+            KV<String, String> out_kv = KV.<String, String>of("", "beam-rule-engine");
+            c.output(out_kv);
         }
     }
 
